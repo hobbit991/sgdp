c839b618f43d6ecbf774b8c0e106362bd10fcb12
hobbit991 <119974158+hobbit991@users.noreply.github.com>
2023-01-22 01:14:18 +0100

    Add files via upload

---
+++ src/test/java/com/sgd/sgd/SGDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgd.sgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SGDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
